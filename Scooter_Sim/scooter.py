9f5eca6664bf4b2bbb0a5e9d5f55e33f921f9e56
nicola-ferrarese <f.nicola98@gmail.com>
2024-03-24 20:07:31 +0100

    [Scooter] - basic functionalities done, signal handling done. TODO: define specific commands and behaviours, currently only start and stop

--- Scooter_Sim/scooter.py
+++ Scooter_Sim/scooter.py
@@ -41,14 +41,17 @@ class Route:
             return None
 
 def run_consumer(scooter, loop):
-    kafka_config = {'bootstrap.servers': 'localhost:9092', 'group.id': 'scooter_group'}
+    kafka_config = {'bootstrap.servers': 'localhost:9092', 'group.id': f"scooters_consumers{random.randint(1, 1000)}", }
     consumer = Consumer(kafka_config)
     consumer.subscribe(['scooter_commands'])
-    print("Consumer started.")
+    print("->Consumer started.")
     try:
         while True:
             msg = consumer.poll(1.0)
             if msg is None:
+                if stop_event.is_set() is True:
+                    print("Stop event is set.")
+                    break
                 continue
             if msg.error():
                 print(f"Consumer error: {msg.error()}")
@@ -57,17 +60,21 @@ def run_consumer(scooter, loop):
                     continue
                 else:
                     raise KafkaException(msg.error())
+            if stop_event.is_set() is True:
+                print("Stop event is set.")
+                break
             else:
-                # Successfully received a message, add it to the command queue
                 message = msg.value().decode('utf-8')
                 message = json.loads(message)
-                #print(f"Received message: {message}")
-                if message["status"] is not None:
-                    print(f"Received status: {message['status']}")
-                    asyncio.run_coroutine_threadsafe(scooter.command_queue.put(message['status']), loop)
-                #print(f"Added command to queue: {msg.value().decode('utf-8')}")
-    finally:
-        consumer.close()
+                if "id" in message.keys() and message['id'] == scooter.id:
+                    if "command" in message.keys() and message["command"] is not None:
+                        print(f"Received command: {message['command']}")
+                        asyncio.run_coroutine_threadsafe(scooter.get_command_queue().put(message['command']), loop)
+        print(f"[{scooter.get_id()}] Consumer stopped.")
+    except KeyboardInterrupt:
+        print("Keyboard interrupt in consumer.")
+
+
 class Scooter:
     def __init__(self, id, producer, topic, graph, command_queue, loop):
         self.id = id
@@ -77,32 +84,87 @@ class Scooter:
         self.route = None
         self.status = 'idle'  # Possible states: idle, navigating, stopped
         self.remaining_distance = 0
-        self.command_queue = command_queue
+        self.command_queue: Queue = command_queue
         self.loop = loop
 
+    def get_command_queue(self):
+        return self.command_queue
 
+    def get_id(self):
+        return self.id
 
-    def run_consumer_in_thread(self,loop):
+    def run_consumer_in_thread(self, loop):
         print("Starting consumer thread.")
-        #executor = ThreadPoolExecutor(max_workers=1)
-        #await self.loop.run_in_executor(executor, run_consumer(self))
-        thread = threading.Thread(target=run_consumer, args=(self,loop))
+        thread = threading.Thread(target=run_consumer, args=(self, loop))
         thread.start()
         print("Consumer thread started.")
-    async def process_commands(self):
+
+    async def process_commands___q(self):
         while True:
+            print("-> Scooter process commands.")
             command = await self.command_queue.get()
             # Process command here...
             print(f"Scooter {self.id} received command: {command}")
             # Example command processing logic
-            if command == 'start' and self.status == 'idle':
+            if command == 'start' and self.status == 'idle' and stop_event.is_set() is False:
+                print(f"[{self.id}] -> start command in queue. Starting navigation.")
                 # For simplicity, hardcoded start and end nodes. In practice, these would be dynamic.
-                asyncio.create_task(self.start_navigation(choose_random_node(self.graph), choose_random_node(self.graph)))
-                #self.start_navigation(choose_random_node(self.graph), choose_random_node(self.graph))
-                print(f"Scooter {self.id} started navigation.")
-            elif command == 'stop':
-                self.stop_navigation()
-                print(f"Scooter {self.id} stopped navigation.")
+                asyncio.run_coroutine_threadsafe(self.start_navigation(choose_random_node(self.graph),
+                                                                       choose_random_node(self.graph)), self.loop)
+                print(f"[{self.id}] -> start navigation started.")
+            elif command == 'stop' or stop_event.is_set() is True:
+                print(f"[{self.id}] -> stop command in queue. Stopping navigation.")
+                asyncio.run_coroutine_threadsafe(self.stop_navigation(), self.loop)
+
+    import asyncio
+
+    async def process_commands(self):
+        while True:
+            # Create a task for getting a command from the queue
+            command_task = asyncio.create_task(self.command_queue.get())
+
+            # Create a task that waits for the stop event to be set
+            stop_task = asyncio.create_task(self.wait_for_stop())
+
+            # Wait for either the command to be received or the stop event to be set
+            done, pending = await asyncio.wait(
+                {command_task, stop_task},
+                return_when=asyncio.FIRST_COMPLETED)
+
+            # If the stop task is done, break the loop and terminate
+            if stop_task in done:
+                print("Stop event set. Terminating process_commands loop.")
+                break
+
+            # Otherwise, process the command
+            if command_task in done:
+                command = command_task.result()  # Get the result of the command task
+                print(f"Scooter {self.id} received command: {command}")
+                # Example command processing logic
+                if command == 'start' and self.status == 'idle' and stop_event.is_set() is False:
+                    print(f"[{self.id}] -> start command in queue. Starting navigation.")
+                    # For simplicity, hardcoded start and end nodes. In practice, these would be dynamic.
+                    asyncio.run_coroutine_threadsafe(self.start_navigation(choose_random_node(self.graph),
+                                                                           choose_random_node(self.graph)), self.loop)
+                    print(f"[{self.id}] -> start navigation started.")
+                elif command == 'stop' or stop_event.is_set() is True:
+                    print(f"[{self.id}] -> stop command in queue. Stopping navigation.")
+                    asyncio.run_coroutine_threadsafe(self.stop_navigation(), self.loop)
+
+            # Cancel any pending tasks
+            for task in pending:
+                task.cancel()
+
+    async def wait_for_stop(self):
+        #logic to wait for the stop event
+        await stop_event.wait()
+        return 'stop'
+
+    # Example stop_event, replace with your actual event handling
+    stop_event = asyncio.Event()
+
+    def add_to_queue(self, command):
+        self.command_queue.put_nowait(command)
 
     def json_serializer(self, data):
         """Converts data to a JSON string and encodes it to bytes."""
@@ -120,7 +182,7 @@ class Scooter:
             }
             serialized_data = self.json_serializer(data)
             self.producer.produce(self.topic, value=serialized_data)
-            #self.producer.flush()
+            # self.producer.flush()
 
     async def start_navigation(self, start_node, end_node):
         self.status = 'navigating'
@@ -130,14 +192,23 @@ class Scooter:
         self.remaining_distance = nx.shortest_path_length(self.graph, start_node, end_node, weight='length')
         self.send_coordinates()  # Send initial coordinates
 
+        print(f"[{self.id}] -> navigate start navigation.")
+        await asyncio.create_task(self.navigate())
+        print(f"[{self.id}] -> navigate closed stopped navigation.")
+
+    async def navigate(self):
         while self.status == 'navigating':
+            print(f"[{self.id}] -> navigating.")
             await asyncio.sleep(1)
-            print("-> Making move")
-            self.make_step()
+            if stop_event.is_set() is False:
+                self.make_step()
+            else:
+                await self.command_queue.put('stop')
+                break
 
-
-    def stop_navigation(self):
-        self.status = 'stopped'
+    async def stop_navigation(self):
+        print(f"[{self.id}] -> stop navigation.")
+        self.status = 'idle'
         self.send_coordinates()  # Send final coordinates
 
     def make_step(self):
@@ -151,18 +222,6 @@ class Scooter:
                 self.send_coordinates()
             else:
                 self.stop_navigation()
-
-    async def process_commandFFs(self, command_queue):
-        while self.status != 'stopped':
-            if not command_queue.empty():
-                command = await command_queue.get()
-                if command == 'start' and self.status == 'idle':
-                    # For simplicity, hardcoded start and end nodes. In practice, these would be dynamic.
-                    self.start_navigation(choose_random_node(self.graph), choose_random_node(self.graph))
-                elif command == 'stop':
-                    self.stop_navigation()
-            await asyncio.sleep(1)  # Prevent this loop from hogging the CPU
-
 
 async def load_graph(file_path):
     return ox.load_graphml(file_path)
@@ -204,41 +263,7 @@ def send_coordinates(producer, topic, route: Route):
     producer.flush()
 
 
-def receive_coordinates(config, topics, stop_event):
-    # Create a Consumer instance
-    consumer = Consumer(config)
-    print('Consumer created')
-    try:
-        # Subscribe to topics
-        consumer.subscribe(topics)
-
-        # Continuously poll for messages
-        while not stop_event.is_set():
-            msg = consumer.poll(timeout=1.0)
-            if msg is None:  # No message returned within timeout
-                continue
-            if msg.error():
-                if msg.error().code() == KafkaException._PARTITION_EOF:
-                    # End of partition event
-                    print(f'End of partition reached {msg.topic()}[{msg.partition()}] @ {msg.offset()}')
-                elif msg.error():
-                    raise KafkaException(msg.error())
-            else:
-                # Successfully received a message
-                print(f'Received message: {msg.value().decode("utf-8")}')
-
-    finally:
-        print('Closing consumer')
-        # Close down consumer to commit final offsets.
-        consumer.close()
-
-
-
 async def main(stop_event, graph_file_path="cesena.graphml"):
-    # Load the graph (this could be a simplified example; adjust as necessary for your use case)
-    #graph = ox.graph_from_place('Piedmont, California, USA', network_type='drive')
-
-
     print("Enter Main.")
     loop = asyncio.get_event_loop()
     producer = Producer({'bootstrap.servers': 'localhost:9092'})
@@ -248,24 +273,22 @@ async def main(stop_event, graph_file_path="cesena.graphml"):
     print("Scooter start init.")
     # Initialize scooters (for simplicity, starting and ending nodes are not dynamically chosen here)
     scooters = [
-        Scooter(id='Scooter1', producer=producer, topic='scooter_commands', graph=graph, command_queue=command_queue, loop=asyncio.get_running_loop()),]
+        Scooter(id='1_1', producer=producer, topic='scooter_commands', graph=graph, command_queue=Queue(),
+                loop=asyncio.get_running_loop()),
+        Scooter(id='1_2', producer=producer, topic='scooter_commands', graph=graph, command_queue=Queue(),
+                loop=asyncio.get_running_loop())]
+
     print("Scooter init done.")
     # Start listening for commands in each scooter
     for scooter in scooters:
-        scooter.run_consumer_in_thread(loop = asyncio.get_running_loop())
+        scooter.run_consumer_in_thread(loop=asyncio.get_running_loop())
     print("Scooter started listening.")
     # Process commands in each scooter
     tasks = [asyncio.create_task(scooter.process_commands()) for scooter in scooters]
     loop.add_signal_handler(signal.SIGINT, signal_handler, loop, stop_event, tasks)
 
-    # Set up Kafka producer (ensure to configure as per your Kafka setup)
-    #producer = Producer({'bootstrap.servers': 'localhost:9092'})
-
-
-
-    # Example commands (in a real scenario, these would come from user input or another part of the application)
     await asyncio.sleep(1)  # Simulate some delay
-    await command_queue.put('start')  # Command to start navigating
+    print("HAndler set, ready to start")
 
     # Wait for all tasks to complete (in this case, they should complete after receiving the 'stop' command)
     await asyncio.gather(*tasks)
@@ -273,7 +296,7 @@ async def main(stop_event, graph_file_path="cesena.graphml"):
 
 if __name__ == '__main__':
 
-    #asyncio.run(main())
+    # asyncio.run(main())
     async def graceful_shutdown(loop, stop_event, tasks):
         print("Graceful shutdown initiated.")
         stop_event.set()  # Signal all tasks to terminate
@@ -285,11 +308,11 @@ if __name__ == '__main__':
     def signal_handler(loop, stop_event, tasks):
         print("Signal received, initiating graceful shutdown.")
         asyncio.ensure_future(graceful_shutdown(loop, stop_event, tasks), loop=loop)
-    # Corrected approach:
+
+
     loop = asyncio.get_event_loop()
     stop_event = asyncio.Event()
 
-    # Register the signal handler
     loop.create_task(main(stop_event))
 
     try:
