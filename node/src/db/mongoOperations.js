e92a593d4edf3c40e9310ca3133c89fd75afaf89
nicola-ferrarese <f.nicola98@gmail.com>
2024-05-10 19:38:19 +0200

    [node] - Scooter info and trip information, updated dependencies

--- node/src/db/mongoOperations.js
+++ node/src/db/mongoOperations.js
@@ -1,6 +1,9 @@
 const { getDb } = require('./mongoConnection');
 const Scooter = require('../models/scooterModel');
+const TripSegment = require('../models/tripModel');
+const uuid = require('uuid');
 const {sendKafkaMessage} = require("../kafka/producer");
+
 
 
 async function createScooter(data) {
@@ -16,7 +19,70 @@ async function updateScooter(id, updateData) {
     const io = require('../middleware/socket').getIO();
     console.log(`Sending update to scooter ${id}: ${JSON.stringify(updateData)}`);
     io.emit('scooterUpdate', updateData);
-    return Scooter.findOneAndUpdate({"id": id}, updateData, {new: true});
+
+    try {
+        const data = {
+            location: {
+                longitude: updateData.lon,
+                latitude: updateData.lat
+            }
+        }
+        if (updateData.lat !== undefined) {
+            await Scooter.findOneAndUpdate({id: id}, data, {upsert: true, new: true, strict: false});
+        }
+    } catch (error) {
+        console.error('Failed to update scooter:', error);
+    }
+
+}
+
+async function updateTrip(id, updateData) {
+    const io = require('../middleware/socket').getIO();
+    console.log(`Sending update to trip ${id}: ${JSON.stringify(updateData)}`);
+    io.emit('tripUpdate', updateData);
+    try {
+        const event = updateData.event;
+        if (event === 'start'){
+            const tripId = uuid.v4();
+            const trip = new TripSegment({
+                scooterId: id,
+                tripId: tripId,
+                start: {
+                    longitude: updateData.start.lon,
+                    latitude: updateData.start.lat
+                },
+                timestamp: updateData.timestamp
+            });
+            // perform trip.save and save the _id
+            const _id = (await trip.save())._id;
+            await Scooter.findOneAndUpdate({id: id}, {currentTripId: tripId, status: 'in-use'}, {upsert: true, new: true, strict: false});
+        }
+        else if (event === 'end') {
+            await Scooter.findOneAndUpdate({id: id}, {currentTripId: null, status: 'available'}, {upsert: true, new: true, strict: false});
+        }
+        else if (event === 'update') {
+            let tripId = await Scooter.findOne({id: id}).select('currentTripId -_id');
+            tripId = tripId.currentTripId;
+            console.log(tripId)
+            await new TripSegment( {
+                scooterId: id,
+                tripId: tripId,
+                timestamp: updateData.timestamp,
+                start: {
+                    longitude: updateData.start.lon,
+                    latitude: updateData.start.lat
+                },
+                end: {
+                    longitude: updateData.end.lon,
+                    latitude: updateData.end.lat
+                },
+                distance: updateData.distance,
+                cost: updateData.distance * 0.5
+            }).save();
+        }
+    } catch (error) {
+        console.error('Failed to update trip:', error);
+    }
 }
 
 async function sendDestination(id, destination) {
@@ -28,4 +94,4 @@ async function deleteScooter(id) {
     return await Scooter.findByIdAndDelete(id);
 }
 
-module.exports = { createScooter, readScooter, deleteScooter, updateScooter, sendDestination };
+module.exports = { createScooter, readScooter, deleteScooter, updateScooter, sendDestination, updateTrip };
